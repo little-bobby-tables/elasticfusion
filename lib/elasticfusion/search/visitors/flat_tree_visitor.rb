ee81d4f3daa87f156e008fe1ff3c22af7679d3ae
little-bobby-tables <little-bobby-tables@users.noreply.github.com>
2017-02-07 16:58:06 +0700

    tree flattening

--- lib/elasticfusion/search/visitors/flat_tree_visitor.rb
+++ lib/elasticfusion/search/visitors/flat_tree_visitor.rb
@@ -2,8 +2,8 @@ require 'elasticfusion/search/visitor'
 
 module Elasticfusion
   module Search
-    # FlatTreeVisitor is a base class for visitors that
-    # require leaf nodes with a common operator to be flattened, e.g.
+    # FlatTreeVisitor is a base class for visitors that require
+    # sub-trees with a common operator to be flattened to lists, e.g.
     #
     #    and                     and
     #  /     \                 /     \
@@ -17,8 +17,18 @@ module Elasticfusion
         super(flatten(node))
       end
 
-      def flatten(node)
-
+      def flatten(node, parent: nil)
+        case node
+          when NegatedClause
+            node.body = flatten(node.body)
+          when Expression
+            node.left = flatten(node.left, parent: node)
+            node.right = flatten(node.right, parent: node)
+            if parent && node.op && node.op == parent.op
+              return [node.left, node.right].flatten
+            end
+        end
+        node
       end
     end
   end
